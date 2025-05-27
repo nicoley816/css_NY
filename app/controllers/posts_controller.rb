a1b844bee6d0aa16ec8bb670d48ad2a1c3814cad
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-05-27 16:11:38 +0000

    followed along

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    # render :template => "posts/index"
+    # render :template => "posts/index" already know this
 
     # alternative responses to requests other than HTML
-    # respond_to do |format|
-    #   format.html # implicitly renders template posts/index.html.erb
-    #   format.json do
-    #     render :json => @posts
-    #   end
-    # end
+    respond_to do |format| # in our response, format.html
+      format.html # implicitly renders template posts/index.html.erb
+      format.json do # when get request in the format of json, let's render it as json
+        render :json => @posts # take posts array, builds up that structure
+      end
+    end
   end
 
   def new
@@ -30,11 +30,11 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
